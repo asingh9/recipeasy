07ba0cd76a6fda08b3778ffe4126c22a4010f0f2
Avi <absingh696@gmail.com>
2021-02-09 00:18:57 -0500

    update server config

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT));
 
 app.use('/', express.static(path.join(__dirname, 'app')));
 app.use(bodyParser.json());
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Additional middleware which will set headers that we need on each request.
 app.use(function(req, res, next) {
     // Set permissive CORS header - this allows this server to be used only as
-    // an API server in conjunction with something like webpack-dev-server.
+    // an API server in conjunction with something like webpack-dev-server. 
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Disable caching so we'll always get the latest comments.
